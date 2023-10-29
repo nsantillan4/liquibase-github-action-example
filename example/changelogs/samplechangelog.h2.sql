739117fe44a1e39ef67653dc9afb00763766a307
Nieves Santillan <nsantillan@comunytek.com>
2023-10-29 20:11:33 +0100

    my changes

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset your.name:1
+--changeset nsc:1
 --rollback DROP TABLE person;
 create table person (
     id int primary key,
@@ -10,7 +10,7 @@ create table person (
     city varchar(30)
 )
 
---changeset your.name:2
+--changeset nsc:2
 --rollback DROP TABLE company;
 create table company (
     id int primary key,
@@ -20,14 +20,24 @@ create table company (
     city varchar(30)
 )
 
---changeset other.dev:3
+--changeset nsc:3
 --rollback ALTER TABLE person DROP COLUMN country;
 alter table person add column country varchar(2)
 
---changeset other.dev:4
+--changeset nsc:4
 --rollback ALTER TABLE person DROP COLUMN state;
 alter table person add column state varchar(2)
 
---changeset other.dev:5
+--changeset nsc:5
 --rollback ALTER TABLE company DROP COLUMN country;
 alter table company add column country varchar(2)
+
+--changeset nsc:6
+--rollback DROP TABLE company;
+create table company2 (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
