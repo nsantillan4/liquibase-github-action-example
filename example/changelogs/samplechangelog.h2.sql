1e1090eaef26e455054d5aec382b2172a3769c07
Nieves Santillan <nsantillan@comunytek.com>
2023-10-29 20:14:57 +0100

    my changes

--- example/changelogs/samplechangelog.h2.sql
+++ example/changelogs/samplechangelog.h2.sql
@@ -41,3 +41,13 @@ create table company2 (
     address2 varchar(50),
     city varchar(30)
 )
+
+--changeset nsc:7
+--rollback DROP TABLE company;
+create table company3 (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
